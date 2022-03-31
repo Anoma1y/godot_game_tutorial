cee83f98be9e0aa0e3a5a20f1bd0f7be7e1e332a
German Voytekhovich <ueuknjg@gmail.com>
2022-04-01 01:31:33 +0300

    add enemy

--- Player/Player.gd
+++ Player/Player.gd
@@ -18,9 +18,11 @@ var roll_vector = Vector2.LEFT
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
+onready var swordHitbox = $HitboxPivot/SwordHitbox
 
 func _ready():
 	animationTree.active = true
+	swordHitbox.knockback_vector = roll_vector
 
 func _process(delta):
 	match state:
@@ -42,6 +44,7 @@ func move_state(delta):
 	
 	if input_vector != Vector2.ZERO:
 		roll_vector = input_vector
+		swordHitbox.knockback_vector = input_vector
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
 		animationTree.set("parameters/Attack/blend_position", input_vector)
@@ -73,6 +76,7 @@ func move():
 	velocity = move_and_slide(velocity)
 
 func roll_animation_finished():
+	velocity = velocity * 0.8
 	state = MOVE
 
 func attack_animation_finished():
